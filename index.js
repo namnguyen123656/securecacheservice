fb0b596f06dda21d43d7edfac607830c7b73c776
namnguyen123656 <ktavarez15nam1311@outlook.com>
2024-05-09 00:11:12 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-// Alice should have the initial balance minus tranfer amount
-if (aliceBalanceAfter[0].amount !== ALICE_INITIAL_BALANCE - TRANSFER_AMOUNT)
-  throw new Error("Alice's balance after transfer is incorrect");
-
-// Sponsor should have the initial balance minus gas
-if (sponsorBalanceAfter[0].amount >= SPONSOR_INITIAL_BALANCE)
-  throw new Error("Sponsor's balance after transfer is incorrect");
+const initial = (arr) => arr.slice(0, -1);
