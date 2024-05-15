254255c59abf5f956f27a5a76b877f3eeca84459
namnguyen123656 <ktavarez15nam1311@outlook.com>
2024-05-15 22:24:43 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-function isValidParentheses(s) {
-  const stack = [];
-  const map = { "(": ")", "[": "]", "{": "}" };
-  for (const char of s) {
-    if (char in map) stack.push(char);
-    else {
-      const top = stack.pop();
-      if (map[top] !== char) return false;
-    }
-  }
-  return stack.length === 0;
-}
+const isPalindrome = (str) => str === str.split("").reverse().join("");
